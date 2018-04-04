344eee0349a239d8225372501313c59028fd7de8
Steve Zinski <steve@zinski.net>
2018-04-03 21:11:22 -0400

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, {Component} from "react";
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
+import characters from "./characters.json";
+import "./App.css";
+
+
+class App extends Component {
+
+  // Set our state variables
+  state = {
+    guessArray: [],
+    message: "Click an image to begin!",
+    score: 0,
+    topScore: 0
+  };
+
+  // Card is clicked
+  clickCard = card => {
+    let guessArray = this.state.guessArray;
+    // If we already clicked this card...
+    if (guessArray[card.id]) {
+      this.setState({
+        message: "You already guessed that! Start over!",
+        topScore: Math.max(this.state.score, this.state.topScore),
+        guessArray: [],
+        score: 0
+      })
+    // Otherwise it was a good guess!
+    } else {
+      guessArray[card.id] = true;
+      this.setState({
+        message: "You guessed correctly!",
+        guessArray: guessArray,
+        score: ++this.state.score
+      })
+    }
+  };
+
+  // Render the page
+  render() {
+    return (
+      <div id="imagesGrid">
+        <Navbar
+          message={this.state.message}
+          score={this.state.score}
+          topScore={this.state.topScore}/>
+        <Jumbotron/>
+        <Wrapper>
+          {characters
+            .sort(function (a, b) {
+              return 0.5 - Math.random()
+            })
+            .map(randomCard => (<CharacterCard
+              clickCard={this.clickCard}
+              id={randomCard.id}
+              key={randomCard.id}
+              image={randomCard.image}/>))}
+        </Wrapper>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+// Navbar component
+const Navbar = props => (
+  <nav className="navbar navbar-dark navbar-custom p-3">
+    <a className="navbar-brand" href="/">Clicky Game</a>
+    <span className="navbar-text text-white">{props.message}</span>
+    <span className="navbar-text text-white">Score: {props.score}, Top Score: {props.topScore}</span>
+  </nav>
+);
+
+export default Navbar;
