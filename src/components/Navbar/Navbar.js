71e589382608c9f01c573d509ea94d8ab6cd6fb9
Steve Zinski <steve@zinski.net>
2018-04-04 11:32:58 -0400

    Fixed broken link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 // Navbar component
 const Navbar = props => (
   <nav className="navbar fixed-top navbar-dark navbar-custom p-3">
-    <a className="navbar-brand" href="/">Clicky Game</a>
+    <span className="navbar-brand">Clicky Game</span>
     <span className="navbar-text text-white">{props.message}</span>
     <span className="navbar-text text-white">Score: {props.score}, Top Score: {props.topScore}</span>
   </nav>
